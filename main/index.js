60314634c8d6e5615a59dc38f61dba59cb377381
Jocelyn <115833657+jjocelynn@users.noreply.github.com>
2023-02-23 21:28:32 -0500

    moved code from cli.js into index.js and deleted cli.js file

--- main/index.js
+++ main/index.js
@@ -1,8 +1,115 @@
-//requiring CLI class from cli.js
-const CLI = require('./lib/cli.js');
+//importing required documents
+const inquirer = require('inquirer');
+const Render = require('./lib/view');
 
-//instantiating a new class
+class CLI {
+    run() {
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "options",
+                message: "\n\nWhat would you like to do?:",
+                choices: [
+                    "view all departments",
+                    "view all roles",
+                    "view all employees",
+                    "add a department",
+                    "add a role",
+                    "add an employee",
+                    "update an employee role",
+                    "quit"]
+            }
+        ])
+            .then((response) => {
+                const render = new Render();
+                if (response.options === "view all departments") {
+                    render.viewDepartmentsTable();
+                    this.run();
+                } else if (response.options === "view all roles") {
+                    render.viewRolesTable();
+                    this.run();
+                } else if (response.options === "view all employees") {
+                    render.viewEmployeesTable();
+                    this.run();
+                } else if (response.options === "add a department") {
+                    return this.newDepartment()
+                } else if (response.options === "add a role") {
+                    return this.addRole()
+                } else if (response.options === "add an employee") {
+                    return this.addEmployee()
+                } else if (response.options === "quit") {
+                    console.log("Bye");
+                    return "quit"
+                }
+            })
+    };
+
+    newDepartment() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "name of new department:"
+            }
+        ]).then((res) => {
+            const render = new Render();
+            render.addDepartment(res.name);
+            this.run();
+        })
+    }
+    addRole() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "title",
+                message: "name of new role:"
+            },
+            {
+                type: "input",
+                name: "salary",
+                message: "salary for this position:"
+            },
+            {
+                type: "input",
+                name: "department",
+                message: "which department does this role belong to?:"
+            },
+        ]).then((res) => {
+            const render = new Render();
+            render.addRole(res.title, res.salary, res.department);
+            this.run();
+        })
+    }
+    addEmployee() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "firstName",
+                message: "first name:"
+            },
+            {
+                type: "input",
+                name: "lastName",
+                message: "last name:"
+            },
+            {
+                type: "input",
+                name: "role",
+                message: "employee's role?:"
+            },
+            {
+                type: "input",
+                name: "manager",
+                message: "employee's manager?:"
+            },
+        ]).then((res) => {
+            const render = new Render();
+            render.addEmployee(res.firstName, res.lastName, res.role, res.manager);
+
+            this.run();
+        })
+    }
+}
+
 const cli = new CLI();
-
-//calling the run function from cli
 cli.run();
