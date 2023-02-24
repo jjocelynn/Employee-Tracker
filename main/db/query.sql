d18c990a0072d3326a25383fb5c9ebb8c76986ae
Jocelyn <115833657+jjocelynn@users.noreply.github.com>
2023-02-23 21:06:18 -0500

    gave other options in the prompt choices functionality

--- main/db/query.sql
+++ main/db/query.sql
@@ -3,9 +3,11 @@ FROM role
 JOIN department
 ON role.department_id = department.id;
 
-SELECT e1.id, e1.first_name AS "first name", e1.last_name AS "last name", role.title AS role, e2.first_name AS manager
+SELECT e1.id, e1.first_name AS "first name", e1.last_name AS "last name", role.title AS "job title", department.name AS department, role.salary, e2.first_name AS manager
 FROM employee e1
 LEFT JOIN role
 ON e1.role_id = role.id
+LEFT JOIN department
+ON role.department_id = department.id
 LEFT JOIN employee e2
 ON e2.id = e1.manager_id;
--- main/lib/view.js
+++ main/lib/view.js
@@ -27,7 +27,7 @@ class Render {
             }
         })
     }
-    
+
     //view all departments
     viewDepartmentsTable() {
         this.tableTemplate('SELECT * FROM department;', 'department');
@@ -40,28 +40,38 @@ class Render {
 
     //view all employees
     viewEmployeesTable() {
-        this.tableTemplate(`SELECT e1.id, e1.first_name AS "first name", e1.last_name AS "last name", role.title AS role, e2.first_name AS manager FROM employee e1 LEFT JOIN role ON e1.role_id = role.id LEFT JOIN employee e2 ON e2.id = e1.manager_id;`, 'employee');
+        this.tableTemplate(`SELECT e1.id, e1.first_name AS "first name", e1.last_name AS "last name", role.title AS "job title", department.name AS department, role.salary, e2.first_name AS manager FROM employee e1 LEFT JOIN role ON e1.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee e2 ON e2.id = e1.manager_id;`, 'employee');
     }
+
+
 
     //ADD/EDIT
 
     // template to add/update code
-    alterTemplate(sqlCode, table) {
+    alterTemplate(sqlCode, errorMessage, successMessage) {
         db.query(sqlCode, (err, results) => {
             if (err) {
-                return console.log('Error selecting from ' + table + "\n\n" + err);
+                return console.log(errorMessage + "\n\n" + err);
             } else {
-                console.log("\n\n\nAll " + table + "'s \n");
-                return console.table(results);
+                return console.log("\n" + successMessage + "\n\n");
             }
         })
     }
 
     //add a department
-
+    addDepartment(name) {
+        this.alterTemplate(`INSERT INTO department (name) VALUES("${name}");`, 'Error in adding department name', 'Successfully added department!')
+    }
     //add a role
+    ///////////////need to find out how to translate user answer into int to match character type
+    addRole(title, salary, department) {
+        this.alterTemplate(`INSERT INTO role (title, salary, department_id) VALUES ("${title}", "${salary}", "${department}");`, 'Error in adding role', 'Successfully added role!')
+    }
 
     //add an employee
+    addEmployee(firstName, lastName, role, manager) {
+        this.alterTemplate(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${firstName}", "${lastName}", "${role}", "${manager}");`, `Error in adding ${firstName} ${lastName}`, `Successfully added ${firstName} ${lastName}!`)
+    }
 
     //update an employee role
 }
